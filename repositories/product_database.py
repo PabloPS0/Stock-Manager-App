82a0f68985916927c4bb6db4e8e0daec639026df
Pablo <pablopierry98@gmail.com>
2024-11-10 23:09:56 -0300

    feat(validation): enforce non-empty values, positive float prices, and positive integer quantities

--- repositories/product_database.py
+++ repositories/product_database.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 from dotenv import load_dotenv
+from service.product_service import ProductValidator
 
 load_dotenv()
 
--- service/product_service.py
+++ service/product_service.py
@@ -1,2 +1,34 @@
+import os, sys
+import customtkinter as ctk
+from controllers import menu
+from repositories.product_database import ProductRepository
+
 class ProductValidator:
-    pass
+    def __init__(self, fields):
+        def validate_non_empty_value(self):
+            
+            # Valida se todos os valores estão preenchidos
+            for fields in self.fields:
+                if not fields.get():
+                    return False
+            return True
+        
+        def validate_positive_price(self, price):
+            # Valida se o preço é um número decimal positivo
+            try:
+                price = float(price)
+                if price <= 0:
+                    return False
+            except ValueError:
+                return False
+            return True
+        
+        def validate_positive_quantity(self, quantity):
+            try:
+                quantity = int(quantity)
+                if quantity <= 0:
+                    return False
+            except ValueError:
+                return False
+            return True
+        
